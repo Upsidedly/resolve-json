0c39073992ad9cce815dc245b9ec2c75bcc4446a
Upsided <matthewlloydw@gmail.com>
2022-03-05 17:21:13 +0000

    Switched to typescript.

--- index.js
+++
@@ -1,9 +0,0 @@
-import { readFile } from 'fs/promises';
-
-/**
-     * Requires a JSON.
-     * @param {string} path
-     * The path of the JSON file.
-     * @returns JSON or provides error
-     */
-export default async (path) => JSON.parse(await readFile(path, 'utf8'))
