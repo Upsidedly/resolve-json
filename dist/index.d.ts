5ec0aff9cd495be9600ff8003e4fb269a5ee2080
Upsided <matthewlloydw@gmail.com>
2022-03-05 17:23:54 +0000

    added dist/ to .gitignore

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare const _default: (path: string) => Promise<any>;
-/**
-     * Requires a JSON.
-     * @param {string} path
-     * The path of the JSON file.
-     * @returns {{[key: string]: any}} JSON or provides error
-     */
-export default _default;
-//# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import { readFile } from 'fs/promises';
-/**
-     * Requires a JSON.
-     * @param {string} path
-     * The path of the JSON file.
-     * @returns {{[key: string]: any}} JSON or provides error
-     */
-export default async (path) => JSON.parse(await readFile(path, 'utf8'));
-//# sourceMappingURL=index.js.map
